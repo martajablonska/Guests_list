428a21eab9b34b85acf73221e5a0f9227ac66503
martajablonska <martawaldon@gmail.com>
2018-12-13 10:08:32 +0100

    Added form to add guest

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,29 @@
     <div class="container">
         <app-event-form></app-event-form>
         <!--progress bar with a number of guests-->
-        <!--form to add guest-->
+        <app-guest-form @guestAdded="addGuestToList"></app-guest-form>
         <!--guests list-->
     </div>
 </template>
 
 <script>
     import EventForm from './components/EventForm.vue';
+    import GuestForm from './components/GuestForm.vue';
     
     export default {
+        data () {
+            return {
+                guests : []
+            };
+        },
         components : {
-            'app-event-form' : EventForm
+            'app-event-form' : EventForm,
+            'app-guest-form' : GuestForm
+        },
+        methods: {
+            addGuestToList() {
+                this.guests.push(guest);
+            }
         }
     }
 </script>
---
+++ src/components/GuestForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-sm-8">
+                <h3>Add new guest to your event</h3>
+                <form>
+                    <div class="form-group">
+                        <label>Guest name</label>
+                        <input type='text' class='form-control' v-model.lazy="guest">
+                    </div> 
+                    <button type='submit' class='btn btn=light' @click.prevent='addGuest'>Add guest</button>
+                </form>    
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                guest: ''
+            }
+        },
+        methods: {
+            addGuest() {
+                this.$emit('guestAdded', this.guest);
+                this.guest = '';
+            }
+        }
+    }
+
+</script>
+
+<style>
+</style>
