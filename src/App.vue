84b1a83e55c7fc2f9628fc94f9af157b0a48b1cd
martajablonska <martawaldon@gmail.com>
2018-12-13 09:12:01 +0100

    Added initialization files

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <!--header with form about event-->
+        <!--progress bar with a number of guests-->
+        <!--form to add guest-->
+        <!--guests list-->
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
